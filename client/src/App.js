cafde44176e8a30d7a6d88ccf022103e38fab440
msteemul <113412804+msteemul@users.noreply.github.com>
2023-04-09 00:00:11 +0400

    added ui plus lint and prettier

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import Navbar from './components/Navbar';
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './components/screens/Home';
+import Login from './components/screens/Login';
+import Signup from './components/screens/Signup';
+import Profile from './components/screens/Profile';
 
 function App() {
   return (
-    <h1>Hello</h1>
-    
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
