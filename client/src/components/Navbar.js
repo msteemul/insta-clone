459f094b562b0117df0cb3bd07ef2fe0c78b4625
msteemul <113412804+msteemul@users.noreply.github.com>
2023-04-10 00:29:04 +0400

    added navbar logout

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,9 +1,10 @@
 import { React, useContext } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { UserContext } from '../App';
 
 const Navbar = () => {
   const { state, dispatch } = useContext(UserContext);
+  const navigate = useNavigate();
   const renderList = () => {
     if (state) {
       return [
@@ -12,6 +13,19 @@ const Navbar = () => {
         </li>,
         <li>
           <Link to="/create">Create Post</Link>
+        </li>,
+        <li>
+          <button
+            className="btn waves-effect waves-light"
+            style={{ margin: '0px 5px 5px 0px' }}
+            onClick={() => {
+              localStorage.clear();
+              dispatch({ type: 'CLEAR' });
+              navigate('/login');
+            }}
+          >
+            Logout
+          </button>
         </li>,
       ];
     } else {
